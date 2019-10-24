0889983e633632e9b585b849ac72c7dc3b63944e
Emily Tucker <emilytucker4@yahoo.com>
2019-10-24 12:42:24 -0400

    added my to my seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name)
+    VALUES("The Original Burger");
+INSERT INTO burgers(burger_name)
+    VALUES("The Comapany Burger");
+INSERT INTO burgers(burger_name)
+    VALUES("The Tennessee Whiskey King Burger");
