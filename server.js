0ff5aa0fa14e22fbcc26c13090d33c24ed1dedcb
Emily Tucker <emilytucker4@yahoo.com>
2019-10-25 11:45:25 -0400

    added to the server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+var exhbs =require("express-handlebars");
+
+app.engine("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_contoller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Ready to go on http://localhost:" + PORT);
+})
