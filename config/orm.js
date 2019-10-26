00efe01aaa6a8b28ccbf2ff8bfdcfd778b6b19ab
Emily Tucker <emilytucker4@yahoo.com>
2019-10-25 23:02:26 -0400

    added the insertOne function

--- config/orm.js
+++ config/orm.js
@@ -32,7 +32,25 @@ selecetAll:function (tableInput, cb) {
         cb(result);
     });
 },
-insertOne
+insertOne:function (table, cols, vals,cb) {
+    var queryString = "INSERT INTO" + table;
+    queryString +="(";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+},
 undateOne
 }
 module.exports = orm;
