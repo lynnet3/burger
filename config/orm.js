699ee4f9a7b2e39d6421257a35733b55ede7c13c
Emily Tucker <emilytucker4@yahoo.com>
2019-10-25 22:59:16 -0400

    made the selectAll function in the orm file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,38 @@
 var connection = require("../config/connection.js");
-var orm ={
 
+function printQuwationMarks(nom) {
+    var arr = [];
+
+    for (var i = 0; i < nom; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+var orm = {
+selecetAll:function (tableInput, cb) {
+    var queryString = "SELCET * FROM" + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+        if (err){
+            throw err;
+        }
+        cb(result);
+    });
+},
+insertOne
+undateOne
 }
 module.exports = orm;
