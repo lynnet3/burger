826b169e4e0cea6878f0fe7e93f0ed6080a9b042
Emily Tucker <emilytucker4@yahoo.com>
2019-12-20 23:01:00 -0500

    made some changes and fixed some paths

--- public/assets/js.js
+++ public/assets/js.js
@@ -1,34 +1,36 @@
-(function(){
-    $(".create-form").on("submit", function(event){
+(function () {
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
 
-        var newBurger ={
+        var newBurger = {
             burger_name: $("#newburger")
-            .val().trim(),
+                .val().trim(),
             devoured: 0
         };
 
-        $.ajax("/api/burgers",{
+        $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
-        }).then(function(){
+        }).then(function () {
             console.log("New Burger Added");
             location.reload();
         });
     });
-    $(".eatburger").on("click", function(event){
+    $(".eatburger").on("click", function (event) {
         event.preventDefault();
 
         var id = $(this).data("id");
         var devouredState = {
             devoured: 1
         };
+        
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function () {
+            console.log("The burger has been DEVOURED");
+            location.reload();
+        })
     });
-    $.ajax("/api/burgers/" + id,{
-        type: "PUT",
-        data: devouredState
-    }).then(function(){
-        console.log("The burger has been DEVOURED");
-        location.reload();
-    })
+
 });
